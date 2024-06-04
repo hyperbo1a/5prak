c1978ea8afbb305ed12dc8a52385dc0395f6fdd2
hyperbo1a <152983476+hyperbo1a@users.noreply.github.com>
2024-06-04 09:06:13 +0300

    Add files via upload

---
+++ 5prak/4prak/Form1.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace _4prak
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            NS_pav = new TextBox();
+            NS_slaptazodis = new TextBox();
+            NS_URL = new TextBox();
+            NS_komentaras = new TextBox();
+            label2 = new Label();
+            NS_sukurti = new Button();
+            P_paieska = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label3 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            P_pavadinimas = new TextBox();
+            label10 = new Label();
+            P_slaptazodis = new TextBox();
+            label11 = new Label();
+            P_URL = new TextBox();
+            label12 = new Label();
+            P_komentaras = new TextBox();
+            label13 = new Label();
+            SA_pavadinimas = new TextBox();
+            label14 = new Label();
+            SA_naujinti = new Button();
+            SA_slaptazodis = new TextBox();
+            label15 = new Label();
+            TS_pavadinimas = new TextBox();
+            label16 = new Label();
+            TS_trinti = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Naujas slaptazodis";
+            // 
+            // NS_pav
+            // 
+            NS_pav.Location = new Point(29, 81);
+            NS_pav.Name = "NS_pav";
+            NS_pav.Size = new Size(133, 23);
+            NS_pav.TabIndex = 2;
+            // 
+            // NS_slaptazodis
+            // 
+            NS_slaptazodis.Location = new Point(29, 138);
+            NS_slaptazodis.Name = "NS_slaptazodis";
+            NS_slaptazodis.Size = new Size(133, 23);
+            NS_slaptazodis.TabIndex = 3;
+            // 
+            // NS_URL
+            // 
+            NS_URL.Location = new Point(29, 196);
+            NS_URL.Name = "NS_URL";
+            NS_URL.Size = new Size(133, 23);
+            NS_URL.TabIndex = 5;
+            // 
+            // NS_komentaras
+            // 
+            NS_komentaras.Location = new Point(29, 254);
+            NS_komentaras.Name = "NS_komentaras";
+            NS_komentaras.Size = new Size(133, 23);
+            NS_komentaras.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(241, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Slaptazodis";
+            // 
+            // NS_sukurti
+            // 
+            NS_sukurti.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            NS_sukurti.Location = new Point(47, 305);
+            NS_sukurti.Name = "NS_sukurti";
+            NS_sukurti.Size = new Size(99, 30);
+            NS_sukurti.TabIndex = 16;
+            NS_sukurti.Text = "Sukurti";
+            NS_sukurti.UseVisualStyleBackColor = true;
+            NS_sukurti.Click += NS_sukurti_Click;
+            // 
+            // P_paieska
+            // 
+            P_paieska.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            P_paieska.Location = new Point(253, 131);
+            P_paieska.Name = "P_paieska";
+            P_paieska.Size = new Size(99, 30);
+            P_paieska.TabIndex = 17;
+            P_paieska.Text = "Paieska";
+            P_paieska.UseVisualStyleBackColor = true;
+            P_paieska.Click += P_paieska_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F);
+            label4.Location = new Point(678, 17);
+            label4.Name = "label4";
+            label4.Size = new Size(145, 25);
+            label4.TabIndex = 18;
+            label4.Text = "Trinti slaptazodi";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14.25F);
+            label5.Location = new Point(399, 17);
+            label5.Name = "label5";
+            label5.Size = new Size(233, 25);
+            label5.TabIndex = 19;
+            label5.Text = "Slaptazodzio atnaujinimas";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(268, 17);
+            label6.Name = "label6";
+            label6.Size = new Size(74, 25);
+            label6.TabIndex = 20;
+            label6.Text = "Paieska";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(480, 315);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 21;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(29, 58);
+            label7.Name = "label7";
+            label7.Size = new Size(91, 20);
+            label7.TabIndex = 22;
+            label7.Text = "Pavadinimas";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(29, 173);
+            label8.Name = "label8";
+            label8.Size = new Size(35, 20);
+            label8.TabIndex = 23;
+            label8.Text = "URL";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(29, 231);
+            label9.Name = "label9";
+            label9.Size = new Size(88, 20);
+            label9.TabIndex = 24;
+            label9.Text = "Komentaras";
+            // 
+            // P_pavadinimas
+            // 
+            P_pavadinimas.Location = new Point(241, 81);
+            P_pavadinimas.Name = "P_pavadinimas";
+            P_pavadinimas.Size = new Size(133, 23);
+            P_pavadinimas.TabIndex = 25;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(241, 58);
+            label10.Name = "label10";
+            label10.Size = new Size(91, 20);
+            label10.TabIndex = 26;
+            label10.Text = "Pavadinimas";
+            // 
+            // P_slaptazodis
+            // 
+            P_slaptazodis.Location = new Point(241, 196);
+            P_slaptazodis.Name = "P_slaptazodis";
+            P_slaptazodis.Size = new Size(133, 23);
+            P_slaptazodis.TabIndex = 27;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(29, 115);
+            label11.Name = "label11";
+            label11.Size = new Size(86, 20);
+            label11.TabIndex = 28;
+            label11.Text = "Slaptazodis";
+            // 
+            // P_URL
+            // 
+            P_URL.Location = new Point(241, 254);
+            P_URL.Name = "P_URL";
+            P_URL.Size = new Size(133, 23);
+            P_URL.TabIndex = 29;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(241, 231);
+            label12.Name = "label12";
+            label12.Size = new Size(35, 20);
+            label12.TabIndex = 30;
+            label12.Text = "URL";
+            // 
+            // P_komentaras
+            // 
+            P_komentaras.Location = new Point(241, 312);
+            P_komentaras.Name = "P_komentaras";
+            P_komentaras.Size = new Size(133, 23);
+            P_komentaras.TabIndex = 31;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label13.Location = new Point(241, 289);
+            label13.Name = "label13";
+            label13.Size = new Size(88, 20);
+            label13.TabIndex = 32;
+            label13.Text = "Komentaras";
+            // 
+            // SA_pavadinimas
+            // 
+            SA_pavadinimas.Location = new Point(442, 81);
+            SA_pavadinimas.Name = "SA_pavadinimas";
+            SA_pavadinimas.Size = new Size(133, 23);
+            SA_pavadinimas.TabIndex = 33;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label14.Location = new Point(442, 58);
+            label14.Name = "label14";
+            label14.Size = new Size(91, 20);
+            label14.TabIndex = 34;
+            label14.Text = "Pavadinimas";
+            // 
+            // SA_naujinti
+            // 
+            SA_naujinti.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            SA_naujinti.Location = new Point(459, 189);
+            SA_naujinti.Name = "SA_naujinti";
+            SA_naujinti.Size = new Size(99, 30);
+            SA_naujinti.TabIndex = 35;
+            SA_naujinti.Text = "Naujinti";
+            SA_naujinti.UseVisualStyleBackColor = true;
+            SA_naujinti.Click += SA_naujinti_Click;
+            // 
+            // SA_slaptazodis
+            // 
+            SA_slaptazodis.Location = new Point(442, 138);
+            SA_slaptazodis.Name = "SA_slaptazodis";
+            SA_slaptazodis.Size = new Size(133, 23);
+            SA_slaptazodis.TabIndex = 36;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label15.Location = new Point(442, 115);
+            label15.Name = "label15";
+            label15.Size = new Size(86, 20);
+            label15.TabIndex = 37;
+            label15.Text = "Slaptazodis";
+            // 
+            // TS_pavadinimas
+            // 
+            TS_pavadinimas.Location = new Point(678, 81);
+            TS_pavadinimas.Name = "TS_pavadinimas";
+            TS_pavadinimas.Size = new Size(133, 23);
+            TS_pavadinimas.TabIndex = 38;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label16.Location = new Point(678, 58);
+            label16.Name = "label16";
+            label16.Size = new Size(91, 20);
+            label16.TabIndex = 39;
+            label16.Text = "Pavadinimas";
+            // 
+            // TS_trinti
+            // 
+            TS_trinti.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            TS_trinti.Location = new Point(694, 131);
+            TS_trinti.Name = "TS_trinti";
+            TS_trinti.Size = new Size(99, 30);
+            TS_trinti.TabIndex = 40;
+            TS_trinti.Text = "Trinti";
+            TS_trinti.UseVisualStyleBackColor = true;
+            TS_trinti.Click += TS_trinti_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(838, 371);
+            Controls.Add(TS_trinti);
+            Controls.Add(label16);
+            Controls.Add(TS_pavadinimas);
+            Controls.Add(label15);
+            Controls.Add(SA_slaptazodis);
+            Controls.Add(SA_naujinti);
+            Controls.Add(label14);
+            Controls.Add(SA_pavadinimas);
+            Controls.Add(label13);
+            Controls.Add(P_komentaras);
+            Controls.Add(label12);
+            Controls.Add(P_URL);
+            Controls.Add(label11);
+            Controls.Add(P_slaptazodis);
+            Controls.Add(label10);
+            Controls.Add(P_pavadinimas);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label3);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(P_paieska);
+            Controls.Add(NS_sukurti);
+            Controls.Add(label2);
+            Controls.Add(NS_komentaras);
+            Controls.Add(NS_URL);
+            Controls.Add(NS_slaptazodis);
+            Controls.Add(NS_pav);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox NS_pav;
+        private TextBox NS_slaptazodis;
+        private TextBox NS_URL;
+        private TextBox NS_komentaras;
+        private Label label2;
+        private Button NS_sukurti;
+        private Button P_paieska;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label3;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox P_pavadinimas;
+        private Label label10;
+        private TextBox P_slaptazodis;
+        private Label label11;
+        private TextBox P_URL;
+        private Label label12;
+        private TextBox P_komentaras;
+        private Label label13;
+        private TextBox SA_pavadinimas;
+        private Label label14;
+        private Button SA_naujinti;
+        private TextBox SA_slaptazodis;
+        private Label label15;
+        private TextBox TS_pavadinimas;
+        private Label label16;
+        private Button TS_trinti;
+    }
+}
